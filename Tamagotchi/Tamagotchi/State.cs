9be97bcbfbfd870131d0c1d8c157fb2aeb7aace5
Stelmashenko-A <stelma_28@bk.ru>
2015-09-28 10:42:33 +0300

    Energy and lifetime were replaced from Tamagotchi to new class State.

---
+++ Tamagotchi/Tamagotchi/State.cs
@@ -0,0 +1,9 @@
+﻿namespace Tamagotchi
+{
+    public class State
+    {
+        public int LiveTime { get; set; }
+
+        public int Energy { get; set; }
+    }
+}
--- Tamagotchi/Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi/Tamagotchi.cs
@@ -4,25 +4,35 @@ namespace Tamagotchi
 {
     public class Tamagotchi
     {
-        public bool IsAlive => _liveTime != 0;
+        private readonly State _state;
+
+        public Tamagotchi()
+        {
+            _state = new State
+            {
+                Energy = 100,
+                LiveTime = 7
+            };
+        }
+
+        public bool IsAlive => _state.LiveTime != 0;
 
         public void TimeDec()
         {
-            _liveTime--;
+            _state.LiveTime--;
         }
-        private int _liveTime = 7;
 
         public bool CanSurvive(int i)
         {
-            return i < _energy;
+            return i < _state.Energy;
         }
 
-        private int _energy = 100;
 
         public void SearchFood(int i)
         {
-            _energy -= i;
+            _state.Energy -= i;
         }
+
         public void PushFood(Food food)
         {
 
